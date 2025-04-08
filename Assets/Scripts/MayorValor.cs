6e8704d5b27e469333cfc5f5fa4cc4478b7c1682
FacundoBenavente <111788429+FacundoBenavente@users.noreply.github.com>
2025-04-08 08:28:46 -0300

    mayor valor empezado

---
+++ Assets/Scripts/MayorValor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayorValor : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public int num3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1 > num2 && num1 > num3){
+            Debug.Log("El mayor numero es num1");
+            } else if(num2 > num1 && num2 > num3){
+            Debug.Log("El mayor numero es num2")
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
